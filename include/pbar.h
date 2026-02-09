bcc8f7498c7e50304d686f49cc6547d675cc5697
Irwindeep <b22ai022@iitj.ac.in>
2026-02-10 04:35:25 +0530

    Change progress bar colors

--- include/pbar.h
+++ include/pbar.h
@@ -2,10 +2,10 @@
 #define PBAR_H
 
 #define COLOR_RESET "\033[0m"
-#define COLOR_GREEN "\033[38;5;82m"
-#define COLOR_BLUE "\033[38;5;75m"
-#define COLOR_GRAY "\033[38;5;245m"
-#define COLOR_YELLOW "\033[38;5;214m"
+#define COLOR_GREEN "\033[92m"
+#define COLOR_BLUE "\033[94m"
+#define COLOR_GRAY "\033[97m"
+#define COLOR_YELLOW "\033[93m"
 
 #define PBAR_INIT_FAILURE 1
 
