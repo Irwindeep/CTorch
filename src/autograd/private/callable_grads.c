6871859e3e8beb223ba47fd1baed9c4a7ec5f70c
Irwindeep <b22ai022@iitj.ac.in>
2026-02-13 19:21:46 +0530

    Compute gradients only for requires_grad tensors

--- src/autograd/private/callable_grads.c
+++ src/autograd/private/callable_grads.c
@@ -43,57 +43,76 @@ _DEFINE_GRAD_FN(_accumulate_grad_fn, 1, 0, {
     replace_tensor_data(tensor_grad, sum);
 })
 
-_DEFINE_GRAD_FN(_add_grad_fn, 1, 2, {
-    Tensor *tensor = inputs[0], *grad = input_grads[0];
-    Tensor *t1 = outputs[0], *t2 = outputs[1];
-    size_t t1_ndim = get_tensor_ndim(t1), t2_ndim = get_tensor_ndim(t2);
-    const size_t *t1_shape = get_tensor_shape(t1),
-                 *t2_shape = get_tensor_shape(t2);
+#define BLOCK(...) {__VA_ARGS__}
 
-    Environment *env = get_tensor_environ(tensor);
-    if (create_graph) {
-        output_grads[0] = broadcast_tensor_grad(grad, t1_ndim, t1_shape);
-        output_grads[1] = broadcast_tensor_grad(grad, t2_ndim, t2_shape);
-    } else {
-        ndArray *data1_grad = copy_array(get_tensor_data(grad)),
-                *data2_grad = copy_array(get_tensor_data(grad));
+#define _ONE_IP_TWO_OP_GRAD_FN(name, CG_T1_BLOCK, CG_T2_BLOCK, NG_T1_BLOCK,    \
+                               NG_T2_BLOCK)                                    \
+    _DEFINE_GRAD_FN(name, 1, 2, {                                              \
+        Tensor *tensor = inputs[0], *grad = input_grads[0];                    \
+        Tensor *t1 = outputs[0], *t2 = outputs[1];                             \
+        ndArray *data1 = get_tensor_data(t1), *data2 = get_tensor_data(t2);    \
+        ndArray *grad_data = get_tensor_data(grad);                            \
+                                                                               \
+        size_t t1_ndim = get_tensor_ndim(t1), t2_ndim = get_tensor_ndim(t2);   \
+        const size_t *t1_shape = get_tensor_shape(t1),                         \
+                     *t2_shape = get_tensor_shape(t2);                         \
+        bool t1_requires_grad = get_requires_grad(t1),                         \
+             t2_requires_grad = get_requires_grad(t2);                         \
+                                                                               \
+        Environment *env = get_tensor_environ(tensor);                         \
+                                                                               \
+        Tensor *t1_grad = NULL, *t2_grad = NULL;                               \
+        if (create_graph) {                                                    \
+            if (t1_requires_grad)                                              \
+                CG_T1_BLOCK                                                    \
+            if (t2_requires_grad)                                              \
+                CG_T2_BLOCK                                                    \
+        } else {                                                               \
+            if (t1_requires_grad)                                              \
+                NG_T1_BLOCK                                                    \
+            if (t2_requires_grad)                                              \
+                NG_T2_BLOCK                                                    \
+        }                                                                      \
+        output_grads[0] = t1_grad;                                             \
+        output_grads[1] = t2_grad;                                             \
+    })
 
+_ONE_IP_TWO_OP_GRAD_FN(
+    _add_grad_fn,
+    BLOCK({ t1_grad = broadcast_tensor_grad(grad, t1_ndim, t1_shape); }),
+    BLOCK({ t2_grad = broadcast_tensor_grad(grad, t2_ndim, t2_shape); }),
+
+    BLOCK({
+        ndArray *data1_grad = copy_array(grad_data);
         data1_grad = broadcast_grad_data(data1_grad, t1_ndim, t1_shape);
+        t1_grad = tensor_init(data1_grad, NO_GRAD, env);
+    }),
+    BLOCK({
+        ndArray *data2_grad = copy_array(grad_data);
         data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
+        t2_grad = tensor_init(data2_grad, NO_GRAD, env);
+    }))
 
-        output_grads[0] = tensor_init(data1_grad, NO_GRAD, env);
-        output_grads[1] = tensor_init(data2_grad, NO_GRAD, env);
-    }
-})
+_ONE_IP_TWO_OP_GRAD_FN(
+    _mul_grad_fn, BLOCK({
+        t1_grad = tensor_mul(t2, grad);
+        t1_grad = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
+    }),
+    BLOCK({
+        t2_grad = tensor_mul(t1, grad);
+        t2_grad = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
+    }),
 
-_DEFINE_GRAD_FN(_mul_grad_fn, 1, 2, {
-    Tensor *tensor = inputs[0], *grad = input_grads[0];
-    Tensor *t1 = outputs[0], *t2 = outputs[1];
-    size_t t1_ndim = get_tensor_ndim(t1), t2_ndim = get_tensor_ndim(t2);
-    const size_t *t1_shape = get_tensor_shape(t1),
-                 *t2_shape = get_tensor_shape(t2);
-
-    Environment *env = get_tensor_environ(tensor);
-    if (create_graph) {
-        Tensor *t1_grad = tensor_mul(t2, grad);
-        Tensor *t2_grad = tensor_mul(t1, grad);
-
-        output_grads[0] = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
-        output_grads[1] = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
-    } else {
-        ndArray *data1 = get_tensor_data(t1), *data2 = get_tensor_data(t2),
-                *grad_data = get_tensor_data(grad);
-
+    BLOCK({
         ndArray *data1_grad = array_mul(data2, grad_data);
-        ndArray *data2_grad = array_mul(data1, grad_data);
-
         data1_grad = broadcast_grad_data(data1_grad, t1_ndim, t1_shape);
+        t1_grad = tensor_init(data1_grad, NO_GRAD, env);
+    }),
+    BLOCK({
+        ndArray *data2_grad = array_mul(data1, grad_data);
         data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
-
-        output_grads[0] = tensor_init(data1_grad, NO_GRAD, env);
-        output_grads[1] = tensor_init(data2_grad, NO_GRAD, env);
-    }
-})
+        t2_grad = tensor_init(data2_grad, NO_GRAD, env);
+    }))
 
 _DEFINE_GRAD_FN(_neg_grad_fn, 1, 1, {
     Tensor *new_tensor = inputs[0], *grad = input_grads[0];
@@ -156,44 +175,42 @@ static void inline _get_dims_for_matmul_grad(Tensor *t, int *dims) {
     dims[ndim - 2] = ndim - 1;
 }
 
-_DEFINE_GRAD_FN(_matmul_grad_fn, 1, 2, {
-    Tensor *tensor = inputs[0], *grad = input_grads[0];
-    Tensor *t1 = outputs[0], *t2 = outputs[1];
-    size_t t1_ndim = get_tensor_ndim(t1), t2_ndim = get_tensor_ndim(t2);
-    const size_t *t1_shape = get_tensor_shape(t1),
-                 *t2_shape = get_tensor_shape(t2);
+_ONE_IP_TWO_OP_GRAD_FN(
+    _matmul_grad_fn, BLOCK({
+        int t2_dims[t2_ndim];
+        _get_dims_for_matmul_grad(t2, t2_dims);
+        Tensor *t2_T = tensor_transpose_env(t2, t2_dims, env);
+        t1_grad = tensor_matmul(grad, t2_T);
+        t1_grad = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
+    }),
+    BLOCK({
+        int t1_dims[t1_ndim];
+        _get_dims_for_matmul_grad(t1, t1_dims);
+        Tensor *t1_T = tensor_transpose_env(t1, t1_dims, env);
+        t2_grad = tensor_matmul(t1_T, grad);
+        t2_grad = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
+    }),
 
-    int t1_dims[t1_ndim], t2_dims[t2_ndim];
-    _get_dims_for_matmul_grad(t1, t1_dims);
-    _get_dims_for_matmul_grad(t2, t2_dims);
+    BLOCK({
+        int t2_dims[t2_ndim];
+        _get_dims_for_matmul_grad(t2, t2_dims);
+        ndArray *data2_T = transpose(data2, t2_dims);
 
-    Environment *env = get_tensor_environ(tensor);
-    if (create_graph) {
-        Tensor *t1_T = tensor_transpose_env(t1, t1_dims, env),
-               *t2_T = tensor_transpose_env(t2, t2_dims, env);
-
-        Tensor *t1_grad = tensor_matmul(grad, t2_T);
-        Tensor *t2_grad = tensor_matmul(t1_T, grad);
-
-        output_grads[0] = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
-        output_grads[1] = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
-    } else {
-        ndArray *data1_T = transpose(get_tensor_data(t1), t1_dims),
-                *data2_T = transpose(get_tensor_data(t2), t2_dims);
-
-        ndArray *data1_grad = matmul(get_tensor_data(grad), data2_T),
-                *data2_grad = matmul(data1_T, get_tensor_data(grad));
-
+        ndArray *data1_grad = matmul(grad_data, data2_T);
         data1_grad = broadcast_grad_data(data1_grad, t1_ndim, t1_shape);
-        data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
-
-        output_grads[0] = tensor_init(data1_grad, NO_GRAD, env);
-        output_grads[1] = tensor_init(data2_grad, NO_GRAD, env);
-
-        free_array(data1_T);
+        t1_grad = tensor_init(data1_grad, NO_GRAD, env);
         free_array(data2_T);
-    }
-})
+    }),
+    BLOCK({
+        int t1_dims[t1_ndim];
+        _get_dims_for_matmul_grad(t1, t1_dims);
+        ndArray *data1_T = transpose(data1, t1_dims);
+
+        ndArray *data2_grad = matmul(data1_T, grad_data);
+        data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
+        t2_grad = tensor_init(data2_grad, NO_GRAD, env);
+        free_array(data1_T);
+    }))
 
 _DEFINE_GRAD_FN(_sum_grad_fn, 1, 1, {
     Tensor *new_tensor = inputs[0], *grad = input_grads[0];
@@ -216,72 +233,58 @@ _DEFINE_GRAD_FN(_sum_grad_fn, 1, 1, {
     }
 })
 
-_DEFINE_GRAD_FN(_max_grad_fn, 1, 2, {
-    Tensor *tensor = inputs[0], *grad = input_grads[0];
-    Tensor *t1 = outputs[0], *t2 = outputs[1];
-    size_t t1_ndim = get_tensor_ndim(t1), t2_ndim = get_tensor_ndim(t2);
-    const size_t *t1_shape = get_tensor_shape(t1),
-                 *t2_shape = get_tensor_shape(t2);
-
-    Environment *env = get_tensor_environ(tensor);
-    if (create_graph) {
-        Tensor *t1_ge_t2 = tensor_ge(t1, t2), *t2_gt_t1 = tensor_gt(t2, t1);
-
-        Tensor *t1_grad = tensor_mul(grad, t1_ge_t2);
-        Tensor *t2_grad = tensor_mul(grad, t2_gt_t1);
-
-        output_grads[0] = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
-        output_grads[1] = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
-    } else {
-        ndArray *data1 = get_tensor_data(t1), *data2 = get_tensor_data(t2);
-        ndArray *data1_ge_data2 = array_ge(data1, data2),
-                *data2_gt_data1 = array_gt(data2, data1);
-
-        ndArray *data1_grad = array_mul(get_tensor_data(grad), data1_ge_data2),
-                *data2_grad = array_mul(get_tensor_data(grad), data2_gt_data1);
-
+_ONE_IP_TWO_OP_GRAD_FN(
+    _max_grad_fn, BLOCK({
+        Tensor *t1_ge_t2 = tensor_ge(t1, t2);
+        t1_grad = tensor_mul(grad, t1_ge_t2);
+        t1_grad = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
+    }),
+    BLOCK({
+        Tensor *t2_gt_t1 = tensor_gt(t2, t1);
+        t2_grad = tensor_mul(grad, t2_gt_t1);
+        t2_grad = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
+    }),
+    BLOCK({
+        ndArray *data1_ge_data2 = array_ge(data1, data2);
+        ndArray *data1_grad = array_mul(grad_data, data1_ge_data2);
         data1_grad = broadcast_grad_data(data1_grad, t1_ndim, t1_shape);
-        data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
 
-        output_grads[0] = tensor_init(data1_grad, NO_GRAD, env);
-        output_grads[1] = tensor_init(data2_grad, NO_GRAD, env);
-
+        t1_grad = tensor_init(data1_grad, NO_GRAD, env);
         free_array(data1_ge_data2);
-        free_array(data2_gt_data1);
-    }
-})
-
-_DEFINE_GRAD_FN(_min_grad_fn, 1, 2, {
-    Tensor *tensor = inputs[0], *grad = input_grads[0];
-    Tensor *t1 = outputs[0], *t2 = outputs[1];
-    size_t t1_ndim = get_tensor_ndim(t1), t2_ndim = get_tensor_ndim(t2);
-    const size_t *t1_shape = get_tensor_shape(t1),
-                 *t2_shape = get_tensor_shape(t2);
-
-    Environment *env = get_tensor_environ(tensor);
-    if (create_graph) {
-        Tensor *t1_le_t2 = tensor_le(t1, t2), *t2_lt_t1 = tensor_lt(t2, t1);
-
-        Tensor *t1_grad = tensor_mul(grad, t1_le_t2);
-        Tensor *t2_grad = tensor_mul(grad, t2_lt_t1);
-
-        output_grads[0] = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
-        output_grads[1] = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
-    } else {
-        ndArray *data1 = get_tensor_data(t1), *data2 = get_tensor_data(t2);
-        ndArray *data1_le_data2 = array_le(data1, data2),
-                *data2_lt_data1 = array_lt(data2, data1);
-
-        ndArray *data1_grad = array_mul(get_tensor_data(grad), data1_le_data2),
-                *data2_grad = array_mul(get_tensor_data(grad), data2_lt_data1);
-
-        data1_grad = broadcast_grad_data(data1_grad, t1_ndim, t1_shape);
+    }),
+    BLOCK({
+        ndArray *data2_gt_data1 = array_gt(data2, data1);
+        ndArray *data2_grad = array_mul(grad_data, data2_gt_data1);
         data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
 
-        output_grads[0] = tensor_init(data1_grad, NO_GRAD, env);
-        output_grads[1] = tensor_init(data2_grad, NO_GRAD, env);
+        t2_grad = tensor_init(data2_grad, NO_GRAD, env);
+        free_array(data2_gt_data1);
+    }))
 
+_ONE_IP_TWO_OP_GRAD_FN(
+    _min_grad_fn, BLOCK({
+        Tensor *t1_le_t2 = tensor_le(t1, t2);
+        t1_grad = tensor_mul(grad, t1_le_t2);
+        t1_grad = broadcast_tensor_grad(t1_grad, t1_ndim, t1_shape);
+    }),
+    BLOCK({
+        Tensor *t2_lt_t1 = tensor_lt(t2, t1);
+        t2_grad = tensor_mul(grad, t2_lt_t1);
+        t2_grad = broadcast_tensor_grad(t2_grad, t2_ndim, t2_shape);
+    }),
+    BLOCK({
+        ndArray *data1_le_data2 = array_le(data1, data2);
+        ndArray *data1_grad = array_mul(grad_data, data1_le_data2);
+        data1_grad = broadcast_grad_data(data1_grad, t1_ndim, t1_shape);
+
+        t1_grad = tensor_init(data1_grad, NO_GRAD, env);
         free_array(data1_le_data2);
+    }),
+    BLOCK({
+        ndArray *data2_lt_data1 = array_lt(data2, data1);
+        ndArray *data2_grad = array_mul(grad_data, data2_lt_data1);
+        data2_grad = broadcast_grad_data(data2_grad, t2_ndim, t2_shape);
+
+        t2_grad = tensor_init(data2_grad, NO_GRAD, env);
         free_array(data2_lt_data1);
-    }
-})
+    }))
