f9d505e5e727860415eef4178c78fc0685107b87
Irwindeep <b22ai022@iitj.ac.in>
2026-02-13 16:49:43 +0530

    Use BLAS transposes

--- src/array/kernel/matmul.c
+++ src/array/kernel/matmul.c
@@ -2,29 +2,26 @@
 #include "ops.h"
 
 #include <cblas.h>
+#include <omp.h>
 #include <stddef.h>
 #include <stdlib.h>
 
+static int num_threads = -1;
+
+static inline int decide_threads(int M, int N, int K) {
+    long flops = 2L * M * N * K;
+
+    if (flops < 5e7)
+        return 1;
+    if (flops < 2e8)
+        return 2;
+    if (flops < 1e9)
+        return 4;
+    return 8;
+}
+
 static inline bool is_contig(size_t rows, size_t cols, size_t sR, size_t sC) {
     return (sR == cols && sC == 1);
-}
-
-static void contig_array_f(const float *src, float *dst, size_t rows,
-                           size_t cols, size_t sR, size_t sC) {
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < cols; j++) {
-            dst[i * cols + j] = src[i * sR + j * sC];
-        }
-    }
-}
-
-static void contig_array_d(const double *src, double *dst, size_t rows,
-                           size_t cols, size_t sR, size_t sC) {
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < cols; j++) {
-            dst[i * cols + j] = src[i * sR + j * sC];
-        }
-    }
 }
 
 void matmul_kernel(const ndArray *arr1, const ndArray *arr2, ndArray *result,
@@ -32,6 +29,13 @@ void matmul_kernel(const ndArray *arr1, const ndArray *arr2, ndArray *result,
     size_t m = get_shape(result)[get_ndim(result) - 2],
            n = get_shape(result)[get_ndim(result) - 1],
            k = get_shape(arr1)[get_ndim(arr1) - 1];
+
+    int threads = decide_threads(m, n, k);
+    if (threads != num_threads) {
+        omp_set_num_threads(threads);
+        openblas_set_num_threads(threads);
+        num_threads = threads;
+    }
 
     DType dtype = get_dtype(result);
     int ndim1 = get_ndim(arr1), ndim2 = get_ndim(arr2), ndim = get_ndim(result);
@@ -50,60 +54,30 @@ void matmul_kernel(const ndArray *arr1, const ndArray *arr2, ndArray *result,
     bool A_contig = is_contig(m, k, sAr, sAc),
          B_contig = is_contig(k, n, sBr, sBc);
 
+    CBLAS_TRANSPOSE transA = A_contig ? CblasNoTrans : CblasTrans;
+    CBLAS_TRANSPOSE transB = B_contig ? CblasNoTrans : CblasTrans;
+
+    int lda = A_contig ? (int)k : (int)m;
+    int ldb = B_contig ? (int)n : (int)k;
+
     switch (dtype) {
     default:
         break;
     case DTYPE_FLOAT: {
-        const float *A0 = (float *)(get_array_data(arr1) + offsetA);
-        const float *B0 = (float *)(get_array_data(arr2) + offsetB);
+        const float *A = (float *)(get_array_data(arr1) + offsetA);
+        const float *B = (float *)(get_array_data(arr2) + offsetB);
         float *C = (float *)(get_array_data(result) + offsetC);
 
-        float *A = (float *)A0;
-        float *B = (float *)B0;
-
-        if (!A_contig) {
-            A = malloc(sizeof(float) * m * k);
-            contig_array_f(A0, A, m, k, sAr, sAc);
-        }
-
-        if (!B_contig) {
-            B = malloc(sizeof(float) * k * n);
-            contig_array_f(B0, B, k, n, sBr, sBc);
-        }
-
-        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (int)m, (int)n,
-                    (int)k, 1.0f, A, (int)k, B, (int)n, 0.0f, C, (int)n);
-
-        if (!A_contig)
-            free(A);
-        if (!B_contig)
-            free(B);
+        cblas_sgemm(CblasRowMajor, transA, transB, (int)m, (int)n, (int)k, 1.0f,
+                    A, lda, B, ldb, 0.0f, C, (int)n);
     } break;
     case DTYPE_DOUBLE: {
-        const double *A0 = (double *)(get_array_data(arr1) + offsetA);
-        const double *B0 = (double *)(get_array_data(arr2) + offsetB);
+        const double *A = (double *)(get_array_data(arr1) + offsetA);
+        const double *B = (double *)(get_array_data(arr2) + offsetB);
         double *C = (double *)(get_array_data(result) + offsetC);
 
-        double *A = (double *)A0;
-        double *B = (double *)B0;
-
-        if (!A_contig) {
-            A = malloc(sizeof(double) * m * k);
-            contig_array_d(A0, A, m, k, sAr, sAc);
-        }
-
-        if (!B_contig) {
-            B = malloc(sizeof(double) * k * n);
-            contig_array_d(B0, B, k, n, sBr, sBc);
-        }
-
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, (int)m, (int)n,
-                    (int)k, 1.0f, A, (int)k, B, (int)n, 0.0f, C, (int)n);
-
-        if (!A_contig)
-            free(A);
-        if (!B_contig)
-            free(B);
+        cblas_dgemm(CblasRowMajor, transA, transB, (int)m, (int)n, (int)k, 1.0f,
+                    A, lda, B, ldb, 0.0f, C, (int)n);
     } break;
     }
 }
