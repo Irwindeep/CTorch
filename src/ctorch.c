437944115e5cda9a1151ca8b5b52af3fb93df9de
Irwindeep <b22ai022@iitj.ac.in>
2026-02-14 00:59:17 +0530

    Remove dynamic thread selection

--- src/ctorch.c
+++ src/ctorch.c
@@ -2,19 +2,12 @@
 #include "random.h"
 #include "tensor.h"
 
-#include <omp.h>
 #include <stdint.h>
 #include <time.h>
 
 PRNG *global_rng;
 
-void CTorchInit() {
-    global_rng = rng_init(time(NULL));
-
-    int ncores = omp_get_num_procs();
-    omp_set_dynamic(0);
-    omp_set_num_threads(ncores);
-}
+void CTorchInit() { global_rng = rng_init(time(NULL)); }
 
 void ManualSeed(uint64_t seed) {
     free_rng(global_rng);
