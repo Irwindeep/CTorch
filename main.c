cb1bff70ed49e0d09e9a83564e3c502722ed080b
Irwindeep <b22ai022@iitj.ac.in>
2026-03-03 20:22:11 +0530

    Update example

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "autograd.h"
 #include "ctorch.h"
 #include "nn.h"
+#include "optim.h"
 #include "pbar.h"
 #include "print.h"
 #include "random.h"
@@ -9,28 +10,13 @@
 
 #include <stdio.h>
 
-void zero_grad_p(size_t num_params, Tensor **params) {
-    for (size_t i = 0; i < num_params; i++)
-        zero_grad(params[i]);
-}
-
-void sgd_step(size_t num_params, Tensor **params, ndArray *lr) {
-    for (size_t i = 0; i < num_params; i++) {
-        ndArray *param_data = get_tensor_data(params[i]);
-        ndArray *params_grad =
-            array_mul(get_tensor_data(get_tensor_grad(params[i])), lr);
-
-        ndArray *new_data = array_sub(param_data, params_grad);
-        replace_tensor_data(params[i], new_data);
-        free_array(params_grad);
-    }
-}
+#define BATCH_SIZE 128
 
 int main() {
     CTorchInit();
     ManualSeed(12);
 
-    Module *model = Sequential(Linear(784, 128), ReLU(), Linear(128, 10));
+    ScopedModule model = Sequential(Linear(784, 128), ReLU(), Linear(128, 10));
 
     size_t num_trainable_vars = num_trainable_variables(model);
     size_t num_non_trainable_vars = num_non_trainable_variables(model);
@@ -38,6 +24,7 @@ int main() {
     size_t num_params = num_parameters(model);
     Tensor *params[num_params];
     parameters(model, params);
+    ScopedOptimizer optim = SGD(num_params, params, 1e-3);
 
     printf("# Trainable Variables: ");
     print_with_commas(num_trainable_vars);
@@ -46,34 +33,28 @@ int main() {
 
     printf("\n%s\n\n", model->repr);
 
-    ndArray *lr = array_init(SHAPE(), DTYPE_FLOAT);
-    populate_array(lr, (const float[]){1e-3});
-
-    int num_batchs = 60000 / 128;
+    int num_batchs = 60000 / BATCH_SIZE;
     char desc[] = "Epoch [1/1]";
     char postfix[128] = "";
     ProgressBar *pbar = progress_init(num_batchs);
     for (int batch = 1; batch <= num_batchs; batch++) {
         ScopedEnvironment env = env_init();
-        zero_grad_p(num_params, params);
+        optim_zero_grad(optim);
 
-        Tensor *x = randn(SHAPE(128, 784), DTYPE_FLOAT, NO_GRAD, env);
+        Tensor *x = randn(SHAPE(BATCH_SIZE, 784), DTYPE_FLOAT, NO_GRAD, env);
         Tensor *y = module_call(model, x);
 
         Tensor *loss = tensor_sub(y, SCALAR_NG(2.0f, env));
         loss = tensor_sum(tensor_mul(loss, loss));
-        loss = tensor_div(loss, SCALAR_NG(128.0f, env));
+        loss = tensor_div(loss, SCALAR_NG((float)BATCH_SIZE, env));
 
         backward(loss, NULL);
-        sgd_step(num_params, params, lr);
+        optim_step(optim);
 
         snprintf(postfix, 128, "Train Loss: %.4f", item(loss).float_val);
         progress_update(pbar, batch, desc, postfix);
     }
     progress_finish(pbar);
-
-    free_array(lr);
-    free_module(model);
 
     CTorchClose();
     return 0;
